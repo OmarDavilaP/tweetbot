6a2d1939d82039653e7abb036fd06253366bb362
Omar Dávila Pérez <omar.davila@ge.com>
2017-10-18 16:23:11 -0500

    removing credentials

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ console.log("The bot is running");
 //var Twit = require('twit');
 
 /*var T = new Twit({
-  consumer_key:         'a1q6CB7wE6NVmstpqF3TrukeQ',
-  consumer_secret:      'XmgADPNSPnZP40qpGpq4DHoV77DR3bqF6kPkMDMXSRUbf7jm2v',
-  access_token:         '2304232742-Rc9pYlQCSFVIMdVjPOTgwCkaAf1597Iq8PeiWHv',
-  access_token_secret:  'bjaA0uC4Nys2EVTfGRNMaq0N34LUTcD7aqMLraEv8qR16',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 });
 
