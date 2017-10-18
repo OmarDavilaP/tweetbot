a7deaceb133b49443b021613f31222a79942994a
OmarDavilaP <morrison.x@gmail.com>
2017-10-18 15:51:36 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("The bot is running");
+var Twit = require('twit');
+
+var T = new Twit({
+  consumer_key:         'a1q6CB7wE6NVmstpqF3TrukeQ',
+  consumer_secret:      'XmgADPNSPnZP40qpGpq4DHoV77DR3bqF6kPkMDMXSRUbf7jm2v',
+  access_token:         '2304232742-Rc9pYlQCSFVIMdVjPOTgwCkaAf1597Iq8PeiWHv',
+  access_token_secret:  'bjaA0uC4Nys2EVTfGRNMaq0N34LUTcD7aqMLraEv8qR16',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
+
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+	if(err){
+		console.log(err);
+		console.log(response);
+	}else{
+		console.log(data);
+	}
+  
+});
